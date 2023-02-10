4a4596d11ad496763721aba8b898212b260ebbc7
CatChow0 <thetrueshibadoggo@gmail.com>
2023-02-10 10:53:02 +0100

    Home page

---
+++ acceuil.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+    <div class="contain-list">
+        <div>
+            <h2>Dernières actualités</h2>
+            <ul class="select-row">
+                <li>
+                    <img class="art-img" src="assets/art1.png" alt="art1">
+                    <h5>ELDEN RING : C'EST 12 MILLIONS DE JEUX VENDUS EN SEULEMENT 3 SEMAINES</h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/art2.png" alt="art2">
+                    <h5>MONSTER HUNTER RISE : L'EXTENSION SUNBREAK REVIENT EN VIDÉO</h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/art3.png" alt="art3">
+                    <h5>GTA 5 : VOICI LE TRAILER DE LANCEMENT</h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/art4.png" alt="art4">
+                    <h5>MINECRAFT S'ASSOCIE AVEC LACOSTE</h5>
+                </li>
+            </ul>
+
+            <h2>Campus</h2>
+            <ul class="select-row">
+                <li>
+                    <img class="art-img" src="assets/cam1.png" alt="campus1" >
+                    <h5>
+                        PROPS ART 3D TEMPS RÉEL PAR BRYAN L.
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/cam2.png" alt="campus2" >
+                    <h5>
+                        MASTERCLASS AVEC UGO BERNALICIS
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/cam3.png" alt="campus3" >
+                    <h5>
+                        PROJET PAO POUR LES ÉTUDIANTS DU BACHELOR MANAGEMENT ET BUSINESS
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/cam4.png" alt="campus4" >
+                    <h5>
+                        VISITE DE L’ATELIER DE KALOUF, STREET ARTIST LYONNAIS
+                    </h5>
+                </li>
+            </ul>
+
+            <h2>Vie associative</h2>
+            <ul class="select-row">
+                <li>
+                    <img class="art-img" src="assets/assos1.png" alt="assos1">
+                    <h5>
+                        ATELIER CUISINE AVEC L’ASSOCIATION KATOCHI
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/assos2.png" alt="assos2">
+                    <h5>
+                        WEEK-END DE BIENVENUE AU GAMING CAMPUS AVEC LE BDE DE LYON
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/assos3.png" alt="assos3">
+                    <h5>
+                        NOCTURNE AU GAMING CAMPUS DE LYON
+                    </h5>
+                </li>
+                <li>
+                    <img class="art-img" src="assets/assos4.png" alt="assos4">
+                    <h5>
+                        HEARTSTONE CHAMPIONSHIP AVEC LE BDE DE LYON
+                    </h5>
+                </li>
+            </ul>
+        
+        </div>
+    </div>
+    
+</body>
+</html>
+    
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 
         <!------------------------------------------CSS/JS LINK---------------------------------------------------------->
-        <link rel="stylesheet" href="css/index.css"/>
+        <link rel="stylesheet" href="css/index.css">
         <link rel="stylesheet" href="css/login.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -56,6 +56,8 @@
         }
         
         // include 'LE FICHIER QUE TU VEUX AFFICHER ENTRE LA NAV ET LE FOOTER QUAND TU ES CO'
+
+        include 'acceuil.php';
 
         include 'partials/footer.php';
         
@@ -101,7 +103,7 @@
     }
     ?>
 
-    <script src="./js/index.js"></script>
+    <script src="js/index.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
